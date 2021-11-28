9eafc0ea9f6e6a54aecf9f814fc9ba7db5eab3b1
Mohand Soluman <72243521+MohandSoluman@users.noreply.github.com>
2021-11-28 19:14:51 +0200

    correct some words

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -6,53 +6,54 @@ import { ProductService } from '../product.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit,OnDestroy {
-pageTitle:string = 'Product List';
-imageWidth:number=100;
-imageMargin:number=2;
-showImage:boolean=false;
-buttonTitle='Show Image';
-private _listFiltter='';
-errorMessage:string = '';
-sub: Subscription | undefined;
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle: string = 'Product List';
+  imageWidth: number = 100;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  buttonTitle = 'Show Image';
+  private _listFilter = '';
+  errorMessage: string = '';
+  sub: Subscription | undefined;
 
-get listFiltter():string{
-  return this._listFiltter;
-}
-set listFiltter(value:string){
-  this._listFiltter=value;
-  this.filteredProducts= this.performFiltered(value);
-}
-filteredProducts:Iproduct[]=[];
-products:Iproduct[]=[]
-  constructor(private productService: ProductService) { }
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.performFiltered(value);
+  }
+  filteredProducts: Iproduct[] = [];
+  products: Iproduct[] = [];
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-   this.sub= this.productService.getProducts().subscribe({
-      next:products => {this.products=products;    
-        this.filteredProducts=this.products;
+    this.sub = this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products = products;
+        this.filteredProducts = this.products;
       },
-      error:err => this.errorMessage=err
+      error: (err) => (this.errorMessage = err),
     });
-
   }
 
   ngOnDestroy(): void {
     this.sub?.unsubscribe();
   }
-  
-toggleImages(){
-    this.showImage=!this.showImage;
-     this.buttonTitle=(this.showImage==true)?'Hide Image':'Show Image';
-}
-performFiltered(filteredBy:string):Iproduct[]{
-  filteredBy=filteredBy.toLocaleLowerCase();
-  return this.products.filter((product:Iproduct)=>
-  product.productName.toLocaleLowerCase().includes(filteredBy));
-}
-onNotify(message:string):void{
-this.pageTitle='Product List: '+message;
-}
+
+  toggleImages() {
+    this.showImage = !this.showImage;
+    this.buttonTitle = this.showImage == true ? 'Hide Image' : 'Show Image';
+  }
+  performFiltered(filteredBy: string): Iproduct[] {
+    filteredBy = filteredBy.toLocaleLowerCase();
+    return this.products.filter((product: Iproduct) =>
+      product.productName.toLocaleLowerCase().includes(filteredBy)
+    );
+  }
+  onNotify(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 }
